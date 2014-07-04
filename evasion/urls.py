4b1b82029bcd78ff6c49923eb2d409e05c1680b8
RK <romainknezevic@gmail.com>
2014-07-04 16:01:13 +0200

    add /contact/ (ajax) and /admin/ urls

--- evasion/urls.py
+++ evasion/urls.py
@@ -3,10 +3,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-# from django.template.loader import add_to_builtins
-# add_to_builtins('django.templatetags.static')
-
-
 urlpatterns = patterns('',
     url(r'^$', 'evasion.views.home', name='home'),
+
+    # ajax
+    url(r'^contact/$', 'evasion.ajax.message', name='contact'),
+
+    # admin
+    url(r'^admin/', include(admin.site.urls)),
 )
