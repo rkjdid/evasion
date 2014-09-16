0fea0b549ced790f62fbccb285d392cf7ac732de
RK <romainknezevic@gmail.com>
2014-09-16 20:38:52 +0200

    add get_range templatetag (for loops in templates)

---
+++ evasion/templatetags/tags.py
@@ -0,0 +1,33 @@
+from django.template import Library
+
+register = Library()
+
+@register.filter
+def get_range(to_value):
+  """
+    Filter - returns a list containing range made from given value
+    Usage (in template):
+
+    <ul>{% for i in 3|get_range %}
+      <li>{{ i }}. Do something</li>
+    {% endfor %}</ul>
+
+    Results with the HTML:
+    <ul>
+      <li>0. Do something</li>
+      <li>1. Do something</li>
+      <li>2. Do something</li>
+    </ul>
+
+    Instead of 3 one may use the variable set in the views
+  """
+  return range(to_value)
+
+@register.filter
+def get_onerange(to_value):
+  return range(1, to_value)
+
+@register.filter
+def get_tworange(to_value):
+  return range(2, to_value)
+
